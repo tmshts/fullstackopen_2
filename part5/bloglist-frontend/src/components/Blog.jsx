523368130e0c4d517ac14632fa80350dcfefd641
tmshts <tomhatas@gmail.com>
2023-11-18 15:33:00 +0100

    working on 5.7 Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+// add state to the blog post like blogFormVisible in Togglable component
+// this state controls the displayed form of the blog post like 
+// url, likes and user
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+
+      {blog.title} {blog.author}
+
+      <div>
+        {blog.url}
+      </div>
+      <div>
+        likes {blog.likes}
+      </div>
+      <div>
+        {blog.user.name}
+      </div>
+
+    </div>
+
+)}
+
 
 export default Blog
