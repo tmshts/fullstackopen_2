a54768ab4d92e9ad3e1ca83d70f901711d6da8fe
tmshts <tomhatas@gmail.com>
2023-11-19 10:55:59 +0100

    5.7 Blog list frontend, step7 done

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 
 // add state to the blog post like blogFormVisible in Togglable component
 // this state controls the displayed form of the blog post like 
 // url, likes and user
 
 const Blog = ({ blog }) => {
+
+  const [blogVisible, setBlogVisible] = useState(false)
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -12,20 +16,31 @@ const Blog = ({ blog }) => {
     marginBottom: 5
   }
 
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+  const handleViewBlog = () => {
+    setBlogVisible(!blogVisible)
+  }
+
   return (
     <div style={blogStyle}>
 
+    <div>
       {blog.title} {blog.author}
+      <button onClick={handleViewBlog}>{blogVisible ? 'hide' : 'view'}</button>
+    </div>
 
+    <div style={showWhenVisible}>
       <div>
         {blog.url}
       </div>
       <div>
-        likes {blog.likes}
+        likes {blog.likes} <button>like</button>
       </div>
       <div>
         {blog.user.name}
       </div>
+    </div>
 
     </div>
 
